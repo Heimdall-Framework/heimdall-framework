753eeb88bb1e5d9187f09eec80332b65a00ee225
Ivan Zlatanov <ivanff1@abv.bg>
2019-08-20 00:38:20 +0300

    Better connection detection was implemented.

--- source/python/analyser.py
+++ source/python/analyser.py
@@ -1,5 +1,8 @@
 import pyudev
 
+ALREADY_CONNECTED_DEVICE_NODE = 'None'
+
+
 def is_being_plugged(device):
     if device.action == "add":
         return True
@@ -10,7 +13,15 @@ context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by(subsystem='usb')
 
+
 while True:
     for device in iter(monitor.poll, None):
         if is_being_plugged(device):
-            print('Device with vendor ID {} and product ID UNKNOWN was plugged.'.format(device.get('ID_VENDOR_FROM_DATABASE'), ))
+            if str(device.device_node) != ALREADY_CONNECTED_DEVICE_NODE:
+                print('A USB device with the following information was connected:')
+                print('Vendor ID: %s' %device.get('ID_VENDOR_FROM_DATABASE'))
+                print('Product model: %s' %device.get('ID_MODEL'))
+                print('Product type: %s' %device.get('DEVTYPE'))
+                print('Main partition label: %s' %device.get('ID_FS_LABEL'))
+                print('Usage: %s' %device.get('ID_FS_USAGE'))
+                
