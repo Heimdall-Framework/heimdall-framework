7038a2f461c729642007151920d6bdc90d99b525
Ivan Zlatanov <ivanff1@abv.bg>
2019-09-11 16:51:07 +0300

    On branch master  Changes to be committed: 	modified:   main.py 	modified:   tests.py

--- source/python/main.py
+++ source/python/main.py
@@ -15,18 +15,40 @@ while True:
         device_list = context.getDeviceList()
         
         if not is_initiating:
+            #if cached devices count is less than the real count of conencted devices
             if cached_devices is None or len(device_list) < len(cached_devices):
+                #caches the currently currently connected devices in a collection
                 cached_devices = device_list
 
+            #if real count of connected devices is more tha the count of the cached ones
             elif len(device_list) > len(cached_devices):
+                #creates a list that contains only the newly connected devices
                 new_devices = dops.find_new_device(cached_devices, device_list)
+                
+                #caches the new devices
                 cached_devices = device_list
-                for device in new_devices:                    
-                    if device.getPortNumber() not in SERVICE_PORTS:
-                        print("Test device was connected. Initiating testing procedure...")
-                        tests.test_device(device, device.getPortNumber())
-                    else:
-                        print("Service device was connected.")
+                
+                #lists the newly connected devices
+                for device in new_devices:                                             
+                    
+                    with usb.USBContext() as context:
+                        handle = context.openByVendorIDAndProductID(device.getVendorID(), device.getProductID(), skip_on_error=True)
+
+                        if handle is None:
+                            print("Device not present, or user is not allowed to use the device.")
+                        else:
+                            if handle.kernelDriverActive(0):
+                                handle.detachKernelDriver(0)
+
+                            handle.claimInterface(0)
+                    
+                        if device.getPortNumber() not in SERVICE_PORTS:
+                            print("Test device was connected. Initiating testing procedure...")
+
+                            tests.test_device(handle, device.getPortNumber())
+                        else:
+                            handle.attachKernelDriver(0)
+                            print("Service device was connected.")
                                      
         else:
             cached_devices = device_list
--- source/python/tests.py
+++ source/python/tests.py
@@ -2,22 +2,14 @@ import usb1 as usb
 import gui_elements as gui
 import device_operations as dops
 
-def test_device(_device, port_number):    
-        print('Device testing initiated.')
+def test_device(device_handle, port_number):    
+        print('>>Device testing initiated.')
 
-        _device = unplugging_test(_device,  port_number)
+        _device = device_handle.getDevice()
 
-        if _device != None:
+        if unplugging_test(_device,  port_number) != None:
                 print('Test 0 passed.')
-
-
-        with usb.USBContext() as context:
-                handle = context.openByVendorIDAndProductID(_device.getVendorID(), _device.getProductID(), skip_on_error=True)
-
-                if handle is None:
-                    print("Device not present, or user is not allowed to use the device.")
-                else:
-                    print('Device interface was claimed.')
+                print('Device interface was claimed.')
 
 def unplugging_test(_device, port_number):
     device_vendor_id = _device.getVendorID()
@@ -37,12 +29,12 @@ def unplugging_test(_device, port_number):
         test_bcdn = _device.getbcdDevice()
 
         if test_vid != device_vendor_id or test_pid != device_product_id or test_bcdn != device_bcd_number:
-            return None
+            return False
 
         #the following line will be removed as part of optimisation when suitable harware is present    
         gui.show_msg_box('Guidline', 'Unlpug the USB device, click Okay and then plug it in tha same port.')
 
-    return _device
+    return True
 
-def io_test_basic (_ device):
-    
+    def io_testing(device_handle):
+        data = device_handle.bulkRead(,1024)
