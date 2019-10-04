05f42bc296212f13af14a829b5c0ec3b6ac51eb5
Ivan Zlatanov <ivanff1@abv.bg>
2019-10-04 12:13:00 +0300

    Initrd finder was included.

--- source/python/analyser.py
+++ source/python/analyser.py
@@ -1,4 +1,6 @@
+import os 
 import filecmp
+
 def is_being_plugged(device):
     if device.action == "add":
         return True
@@ -7,3 +9,9 @@ def is_being_plugged(device):
 
 def compare_files(firstFile, secondFile):
     return filecmp.cmp(firstFile, secondFile)
+
+def find_initrd(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == 'initrd':
+                return '{0}/{1}'.format(root, file)
