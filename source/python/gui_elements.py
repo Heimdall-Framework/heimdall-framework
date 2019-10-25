e1efb930e952ec8f979e8ad88e68a92caae008c3
Ivan Zlatanov <ivanff1@abv.bg>
2019-10-25 22:36:36 +0300

    GUI was refactored but has some bugs.

--- source/python/gui_elements.py
+++ source/python/gui_elements.py
@@ -1,27 +1,34 @@
 import tkinter as tk
+import math
 from pymsgbox import alert
 from usb_detector import USBHotplugDetector
 
-FONT_LARGE = ('Verdona', 14)
+FONT_LARGE = ('Verdona', 16)
+FONT_SMALL = ('Verdona', 12)
+
 
 class HeimdallApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, 'Heimdall')
+
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
         container.rowconfigure(0, weight=1)
         container.columnconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky='nsew')
+
+        for F in (StartPage, ConfigPage):
+            frame = F(container, self)
+            
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
         
         self.show_frame(StartPage)
 
-    def show_frame(self, controller):
-        frame = self.frames[controller]
+    def show_frame(self, container):
+        frame = self.frames[container]
         frame.tkraise()
 
 
@@ -29,40 +36,85 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
+        usb_detector = USBHotplugDetector() 
+
+        # navigation menu:
+        evaluator_page_btn = tk.Button(
+            self,
+            text='Evaluator',
+            command = None
+        )
+        evaluator_page_btn.grid(column=0, row=0)
+
+        config_page_btn = tk.Button(
+            self,
+            text='Configs',
+            command = lambda:controller.show_frame(ConfigPage)
+        )
+        config_page_btn.grid(column=1, row=0)
+        
+        # page content:
         label = tk.Label(
             self, 
             text='Heimdall - Main', 
             font=FONT_LARGE
             )
-        label.pack(pady=5, padx=5)
-        
-        usb_detector = USBHotplugDetector() 
+        label.grid(column=2, row=1)
 
-        startEvaluator = tk.Button(
+        start_evaluator_btn = tk.Button(
             self,
             text='Start Evaluator',
             command = usb_detector.start
             )
-        startEvaluator.pack(pady=5, padx=5)
-
-        stopEvaluator = tk.Button(
+        start_evaluator_btn.grid(column=2, row=2)
+        
+        stop_evaluator_btn = tk.Button(
             self,
             text='Stop Evaluator', 
             command = usb_detector.stop
             )
-        stopEvaluator.pack(pady=5, padx=5)
+        stop_evaluator_btn.grid(column=2, row=3)
+
+        log_label = tk.Label(
+            self, 
+            text='Logs:', 
+            font=FONT_SMALL
+            )
+        log_label.grid(column=0, row=4)
+
+        log_textbox = tk.Text(
+            self,
+            height=40,
+            width= 100
+        )
+        log_textbox.grid(column=0, row=5)
 
 class ConfigPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+       # navigation menu:
+        evaluator_page_btn = tk.Button(
+            self,
+            text='Evaluator',
+            command = lambda: controller.show_frame(StartPage)
+        )
+        evaluator_page_btn.grid(column=0, row=0)
+
+        config_page_btn = tk.Button(
+            self,
+            text='Configs',
+            command = None
+        )
+        config_page_btn.grid(column=1, row=0)
+        
+        # page content:
         label = tk.Label(
             self,
             text='Heimdall - Config', 
             font=FONT_LARGE
             )
-        label.pack(pady=5, padx=5)
-
+        label.grid(column=2, row=1)
 
 def show_msg_box(box_title, box_content, buttons = 'Okay'):
     alert(box_content, box_title, buttons)
--- source/python/logger.py
+++ source/python/logger.py
@@ -1,15 +1,15 @@
 import os 
 from datetime import datetime
 
-DUMP_FILES_DIRECTORY_PATH = '/heimdall_dumps'
+dump_files_directory_path = '/heimdall_dumps'
 
 def log(string):
     current_datetime = datetime.now()
     
-    if not os.path.exists(DUMP_FILES_DIRECTORY_PATH):
-        os.mkdir(DUMP_FILES_DIRECTORY_PATH)
+    if not os.path.exists(dump_files_directory_path):
+        os.mkdir(dump_files_directory_path)
     
-    dumpfile_name = '%s/LOG_%s_%s_%s'%(DUMP_FILES_DIRECTORY_PATH, current_datetime.year, current_datetime.month, current_datetime.day)
+    dumpfile_name = '%s/LOG_%s_%s_%s'%(dump_files_directory_path, current_datetime.year, current_datetime.month, current_datetime.day)
     
     file = open(dumpfile_name + '.log' , 'a+') 
 
