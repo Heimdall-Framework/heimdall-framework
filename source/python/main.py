1c9efb4a8712959b25a17a632d9068ac9c1bce12
Ivan Zlatanov <ivanff1@abv.bg>
2019-10-26 23:43:58 +0300

    GUI was upgraded but it is not working properly.

--- source/python/main.py
+++ source/python/main.py
@@ -1,6 +1,70 @@
-from gui_elements import HeimdallApp
+from usb_detector import USBHotplugDetector
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-# starts the application
-app = HeimdallApp()
-app.attributes('-fullscreen', True)
-app.mainloop()
+class Ui_HeimdallApp(object):
+    def setupUi(self, HeimdallApp):
+        HeimdallApp.setObjectName("HeimdallApp")
+        HeimdallApp.resize(851, 595)
+
+        usb_detector = USBHotplugDetector()
+
+        self.centralwidget = QtWidgets.QWidget(HeimdallApp)
+        self.centralwidget.setObjectName("centralwidget")
+        self.start_evaluator_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.start_evaluator_btn.setGeometry(QtCore.QRect(340, 80, 161, 37))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        
+        self.start_evaluator_btn.setFont(font)
+        self.start_evaluator_btn.setObjectName("start_evaluator_btn")
+        self.stop_evaluator_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.stop_evaluator_btn.setGeometry(QtCore.QRect(340, 140, 161, 37))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        
+        self.stop_evaluator_btn.setFont(font)
+        self.stop_evaluator_btn.setObjectName("stop_evaluator_btn")
+        self.logs_text_box = QtWidgets.QTextEdit(self.centralwidget)
+        self.logs_text_box.setGeometry(QtCore.QRect(10, 270, 831, 311))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        
+        self.logs_text_box.setFont(font)
+        self.logs_text_box.setObjectName("logs_text_box")
+        self.logs_label = QtWidgets.QLabel(self.centralwidget)
+        self.logs_label.setGeometry(QtCore.QRect(20, 240, 64, 31))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        
+        self.logs_label.setFont(font)
+        self.logs_label.setObjectName("logs_label")
+        
+        HeimdallApp.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(HeimdallApp)
+        self.statusbar.setObjectName("statusbar")
+        
+        HeimdallApp.setStatusBar(self.statusbar)
+
+        self.retranslateUi(HeimdallApp)
+        QtCore.QMetaObject.connectSlotsByName(HeimdallApp)
+
+        self.start_evaluator_btn.clicked.connect(usb_detector.start())
+        self.stop_evaluator_btn.clicked.connect(usb_detector.stop())
+
+    def retranslateUi(self, HeimdallApp):
+        _translate = QtCore.QCoreApplication.translate
+        HeimdallApp.setWindowTitle(_translate("HeimdallApp", "Heimdall"))
+        self.start_evaluator_btn.setText(_translate("HeimdallApp", "Start Evaluator"))
+        self.stop_evaluator_btn.setText(_translate("HeimdallApp", "Stop Evaluator"))
+        self.logs_label.setText(_translate("HeimdallApp", "Logs:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    HeimdallApp = QtWidgets.QMainWindow()
+    ui = Ui_HeimdallApp()
+    ui.setupUi(HeimdallApp)
+    HeimdallApp.show()
+    sys.exit(app.exec_())
+
