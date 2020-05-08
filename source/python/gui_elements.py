daaae48c1e50dfba28a06588fcb3d1531a6872af
Ivan Zlatanov <ivanff1@abv.bg>
2020-05-09 00:04:45 +0300

    The new GUI was rendered and implemented. Main GUI is now fully responsive

--- source/python/gui_elements.py
+++ source/python/gui_elements.py
@@ -6,67 +6,132 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 usb_detector = USBHotplugDetector()
-#evaluator_thread = Thread(target=usb_detector.start)
 
-class Ui_HeimdallApp(object):
-    def setupUi(self, HeimdallApp):
+
+class HeimdallMainWindow(object):
+    def setupUi(self, MainWindow):        
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(989, 671)
+
+        small_font = QtGui.QFont()
+        small_font.setPointSize(12)
+
+        large_font = QtGui.QFont()
+        large_font.setPointSize(14)
+
         self.threadpool = QtCore.QThreadPool()
 
-        HeimdallApp.setObjectName("HeimdallApp")
-        HeimdallApp.resize(851, 595)
-
-        sys.stdout = WritingStream(outputed_text=self.normal_write_text)
         
-        self.centralwidget = QtWidgets.QWidget(HeimdallApp)
+        sys.stdout = WritingStream(outputed_text=self.normal_write_text)
+
+
+        MainWindow.setStyleSheet("background-color: rgba(61, 61, 61, 253)")
+        MainWindow.setTabShape(QtWidgets.QTabWidget.Triangular)
+        MainWindow.setUnifiedTitleAndToolBarOnMac(True)
+        
+        icon = QtGui.QIcon()
+        
+        MainWindow.setProperty("icon", icon)
+ 
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setAutoFillBackground(False)
         self.centralwidget.setObjectName("centralwidget")
-
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.main_grid = QtWidgets.QGridLayout()
+        self.main_grid.setHorizontalSpacing(40)
+        self.main_grid.setVerticalSpacing(20)
+        self.main_grid.setObjectName("main_grid")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+ 
+        self.label.setFont(large_font)
+        self.label.setStyleSheet("color: white;")
+        self.label.setObjectName("label")
+        self.main_grid.addWidget(self.label, 3, 0, 1, 1)
+ 
+        spacer_item = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.main_grid.addItem(spacer_item, 0, 3, 1, 1)
+        spacer_item_one = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        self.main_grid.addItem(spacer_item_one, 2, 1, 1, 1)
+        spacer_item_two = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.main_grid.addItem(spacer_item_two, 0, 0, 1, 1)
+        
         self.start_evaluator_btn = QtWidgets.QPushButton(self.centralwidget)
-        self.start_evaluator_btn.setGeometry(QtCore.QRect(340, 80, 161, 37))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.start_evaluator_btn.setFont(font)
-        self.start_evaluator_btn.setObjectName("start_evaluator_btn")
-
-        self.stop_evaluator_btn = QtWidgets.QPushButton(self.centralwidget)
-        self.stop_evaluator_btn.setGeometry(QtCore.QRect(340, 140, 161, 37))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.stop_evaluator_btn.setFont(font)
-        self.stop_evaluator_btn.setObjectName("stop_evaluator_btn")
-
-        self.logs_text_box = QtWidgets.QTextEdit(self.centralwidget)
-        self.logs_text_box.setGeometry(QtCore.QRect(10, 270, 831, 311))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.logs_text_box.setFont(font)
-        self.logs_text_box.setObjectName("logs_text_box")
-
-        self.logs_label = QtWidgets.QLabel(self.centralwidget)
-        self.logs_label.setGeometry(QtCore.QRect(20, 240, 64, 31))
+ 
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        size_policy.setHeightForWidth(self.start_evaluator_btn.sizePolicy().hasHeightForWidth())
+ 
+        self.start_evaluator_btn.setSizePolicy(size_policy)
+        self.start_evaluator_btn.setMinimumSize(QtCore.QSize(200, 50))
+ 
         font = QtGui.QFont()
         font.setPointSize(14)
-        self.logs_label.setFont(font)
-        self.logs_label.setObjectName("logs_label")
+ 
+        self.start_evaluator_btn.setFont(large_font)
+        self.start_evaluator_btn.setStyleSheet("background: grey;\n"
+            "color: white;\n"
+            ""
+            )
         
-        HeimdallApp.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(HeimdallApp)
+        self.start_evaluator_btn.setObjectName("start_evaluator_btn")
+        self.main_grid.addWidget(self.start_evaluator_btn, 0, 1, 1, 1)
+        self.stop_evaluator_btn = QtWidgets.QPushButton(self.centralwidget)
+        
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        size_policy.setHeightForWidth(self.stop_evaluator_btn.sizePolicy().hasHeightForWidth())
+        
+        self.stop_evaluator_btn.setSizePolicy(size_policy)
+        self.stop_evaluator_btn.setMinimumSize(QtCore.QSize(200, 50))
+        
+        self.stop_evaluator_btn.setFont(large_font)
+        self.stop_evaluator_btn.setStyleSheet("background: grey;\n"
+            "color: white;"
+            )
+        
+        self.stop_evaluator_btn.setObjectName("stop_evaluator_btn")
+        self.main_grid.addWidget(self.stop_evaluator_btn, 0, 2, 1, 1)
+        self.logs_text_box = QtWidgets.QTextEdit(self.centralwidget)
+
+       
+        self.logs_text_box.setFont(small_font)
+        self.logs_text_box.setStyleSheet("background: white;\n" 
+        "color: black;"
+        )
+
+        self.logs_text_box.setReadOnly(True)
+        self.logs_text_box.setAcceptRichText(False)
+        self.logs_text_box.setObjectName("logs_text_box")
+        self.logs_text_box.moveCursor(QtGui.QTextCursor.End)
+        self.main_grid.addWidget(self.logs_text_box, 4, 0, 1, 4)
+       
+        self.gridLayout.addLayout(self.main_grid, 1, 0, 1, 1)
+        spacer_item_three = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.gridLayout.addItem(spacer_item_three, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+       
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-        
-        HeimdallApp.setStatusBar(self.statusbar)
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(HeimdallApp)
-        QtCore.QMetaObject.connectSlotsByName(HeimdallApp)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, HeimdallApp):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        HeimdallApp.setWindowTitle(_translate("HeimdallApp", "Heimdall"))
-        self.start_evaluator_btn.setText(_translate("HeimdallApp", "Start Evaluator"))
-        self.stop_evaluator_btn.setText(_translate("HeimdallApp", "Stop Evaluator"))
-        self.logs_label.setText(_translate("HeimdallApp", "Logs:"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "USB Threat Evaluator"))
+        
+        self.label.setText(_translate("MainWindow", "Logs:"))
+        self.start_evaluator_btn.setText(_translate("MainWindow", "Start Evaluator"))
+        self.stop_evaluator_btn.setText(_translate("MainWindow", "Stop Evaluator"))
 
         self.start_evaluator_btn.clicked.connect(self.__start_evaluator)
         self.stop_evaluator_btn.clicked.connect(self.__stop_evaluator)
-    
+
     def normal_write_text(self, text):
         cursor = self.logs_text_box.textCursor()
         cursor.movePosition(QtGui.QTextCursor.End)
@@ -80,8 +145,7 @@ class Ui_HeimdallApp(object):
 
     def __stop_evaluator(self):
         usb_detector.stop()
-        
-    
+
 
 class WritingStream(QtCore.QObject):
     outputed_text = QtCore.pyqtSignal(str)
@@ -98,7 +162,7 @@ def show_gui():
     app = QtWidgets.QApplication(sys.argv)
     HeimdallApp = QtWidgets.QMainWindow()
 
-    ui = Ui_HeimdallApp()
+    ui = HeimdallMainWindow()
     ui.setupUi(HeimdallApp)
 
     HeimdallApp.show()
