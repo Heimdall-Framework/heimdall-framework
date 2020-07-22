54dcfd60d4c2d8d260b0f8bccb3a05387793e701
Ivan Zlatanov <ivanff1@abv.bg>
2020-07-22 15:38:10 +0300

    Deployment testin is initiated.

---
+++ source/release_scripts/release.sh
@@ -0,0 +1,40 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+VERSIONS_FILE = "$DIR/../../version.txt"
+
+MAJOR=$(cut -d "=" -f2 <<< $(sed -n '1p' < "$DIR/../../version.txt"))
+MINOR=$(cut -d "=" -f2 <<< $(sed -n '2p' < "$DIR/../../version.txt"))
+PATCH=$(cut -d "=" -f2 <<< $(sed -n '3p' < "$DIR/../../version.txt"))
+
+function build_version ()
+{
+    if ((PATCH < 20)); then
+        PATCH=$((PATCH+1))
+    elif ((MINOR < 20)); then
+        MINOR=$((MINOR+1))
+        PATCH=0
+    else
+        MAJOR=$((MAJOR+1))
+        MINOR=0
+        PATCH=0
+    fi
+
+    echo $MAJOR"."$MINOR"."$PATCH 
+    echo -e $"MAJOR=$MAJOR\nMINOR=$MINOR\nPATCH=$PATCH" > $VERSIONS_DIR
+}
+
+function create_release_archive ()
+{
+    archive_name="core-$MAJOR.$MINOR.$PATCH.tar.gz"
+    cd "$Dir/../../../"
+    ls
+}
+
+function main ()
+{
+    echo "Building version number..."
+    build_version
+    echo "Building release archive..."
+    create_release_archive
+}
+
+main
