91c85687eb90597ea499e6fd16df13a79d9b0e2d
Ivan Zlatanov <ivanff1@abv.bg>
2020-11-25 18:52:09 +0200

    Core framework buclet was updated.

--- source/release_scripts/clear_bucket.sh
+++ source/release_scripts/clear_bucket.sh
@@ -1,6 +1,6 @@
 function clear_s3_bucket ()
 {
-    aws s3 rm arn:aws:s3:eu-central-1::core-framework-bucket --recursive --exclude="*" --include="*.tar.gz"
+    aws s3 rm s3://core-framework-bucket --recursive --exclude="*" --include="*.tar.gz"
 }
 
 clear_s3_bucket
