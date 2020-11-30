18ce0170c239bde72f79a0df0acc18f49d9eed1a
Ivan Zlatanov <ivanff1@abv.bg>
2020-11-30 14:35:44 +0200

    Some code formatting in main.py

--- source/heimdall_framework/main.py
+++ source/heimdall_framework/main.py
@@ -11,29 +11,31 @@ from .modules.usb_detector import USBHotplugDetector
 from .modules.file_operations_provider import FileOperationsProvider
 from .modules.configuration_deserializer import ConfigurationDeserializer
 
-class Main():
+class Main:
     def __init__(self):
         pass
 
     def main(self) -> None:
         configuration_deserializer = None
         configuration = None
-        
-        parser = argparse.ArgumentParser(description='Load CLI flags for the framework.')
+
+        parser = argparse.ArgumentParser(
+            description="Load CLI flags for the framework."
+        )
         parser.add_argument(
-            '--config',
+            "--config",
             type=str,
-            help='Path to your custom configuration file. If no passed, will load the default one.'
-            )
+            help="Path to your custom configuration file. If no passed, will load the default one.",
+        )
         parser.add_argument(
-            '--interface', 
+            "--interface",
             type=str,
-            nargs='?',
-            help='Indicates whether the GUI will be initialized or not.',
-            )
+            nargs="?",
+            help="Indicates whether the GUI will be initialized or not.",
+        )
         parser.add_argument(
-            '--screen',
-            nargs='?',
+            "--screen",
+            nargs="?",
             type=str,
             help='The GUI mode. Should be "normal", "fullscreen" or left empty.',
         )
@@ -41,43 +43,40 @@ class Main():
         cli_arguments = parser.parse_args()
 
         if cli_arguments.interface == None:
-            print('Interface must be specified.')
+            print("Interface must be specified.")
             return
 
-        print('Loading configuration')
+        print("Loading configuration")
         if cli_arguments.config != None:
             configuration_deserializer = ConfigurationDeserializer(cli_arguments.config)
             configuration = configuration_deserializer.deserialize()
 
-        configuration_deserializer = ConfigurationDeserializer('../configuration.json')
+        configuration_deserializer = ConfigurationDeserializer("../configuration.json")
         configuration = configuration_deserializer.deserialize()
 
         logger = Logger(configuration.logs_directory)
 
         current_file_location = os.path.dirname(os.path.abspath(__file__))
-        framework_location = os.path.abspath(os.path.join(current_file_location, '../../'))
+        framework_location = os.path.abspath(
+            os.path.join(current_file_location, "../../")
+        )
 
-        logger.log('>>> Initiating updater')
-        updater = Updater(
-            configuration, 
-            logger, 
-            framework_location,
-            ''
-            )
+        logger.log(">>> Initiating updater")
+        updater = Updater(configuration, logger, framework_location, "")
 
-        logger.log('>>> Checking for updates.')
+        logger.log(">>> Checking for updates.")
         if updater.can_update(logger):
             updater.update(logger)
-        
-        logger.log('>>> Initiating USB hotplug detector.')
-        if cli_arguments.interface == None or cli_arguments.interface == 'nogui':
+
+        logger.log(">>> Initiating USB hotplug detector.")
+        if cli_arguments.interface == None or cli_arguments.interface == "nogui":
             usb_detector = USBHotplugDetector(configuration, logger)
             usb_detector.start()
         else:
-            if cli_arguments.screen == None or cli_arguments.screen == 'normal':
+            if cli_arguments.screen == None or cli_arguments.screen == "normal":
                 gui.show_gui(configuration, logger, False)
             else:
                 gui.show_gui(configuration, logger, True)
 
-def run(): 
+def run():
     Main().main()
