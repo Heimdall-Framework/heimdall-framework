6c4262f936a13bf5ce2873d2d09c84fd0cf4a60b
Ivan Zlatanov <ivanff1@abv.bg>
2020-12-10 01:51:04 +0200

    Double quotes were changed to single quotes in gui.py

--- heimdall_framework/modules/gui.py
+++ heimdall_framework/modules/gui.py
@@ -17,112 +17,112 @@ class HeimdallMainWindow(object):
         usb_detector = USBHotplugDetector(configuration, logger)
 
     def setupUi(self, USBEvaluatorGui):
-        USBEvaluatorGui.setObjectName("USBEvaluatorGui")
+        USBEvaluatorGui.setObjectName('USBEvaluatorGui')
         USBEvaluatorGui.resize(800, 480)
         USBEvaluatorGui.setStyleSheet(
-            "font: 12pt \"Nirmala UI\";\n"
-            "color: white;\n"
-            "background: #2c3e50;\n"
-            "")
+            'font: {}pt \'Nirmala UI\';\n'
+            'color: white;\n'
+            'background: #2c3e50;\n'
+            ''.format(MEDIUM_FONT))
 
         self.threadpool = QtCore.QThreadPool()
         sys.stdout = WritingStream(outputted_text= self.normal_write_text)
             
         self.sidebar = QtWidgets.QWidget(USBEvaluatorGui)
         self.sidebar.setGeometry(QtCore.QRect(-6, 0, 130, 480))
-        self.sidebar.setStyleSheet("background: #253442;")
-        self.sidebar.setObjectName("sidebar")
+        self.sidebar.setStyleSheet('background: #253442;')
+        self.sidebar.setObjectName('sidebar')
 
         self.about_btn_widget = QtWidgets.QWidget(self.sidebar)
         self.about_btn_widget.setGeometry(QtCore.QRect(0, 113, 130, 111))
-        self.about_btn_widget.setStyleSheet("background: #212c38;")
-        self.about_btn_widget.setObjectName("about_btn_widget")
+        self.about_btn_widget.setStyleSheet('background: #212c38;')
+        self.about_btn_widget.setObjectName('about_btn_widget')
 
         self.question_image_label = QtWidgets.QLabel(self.about_btn_widget)
         self.question_image_label.setGeometry(QtCore.QRect(30, 10, 80, 70))
-        self.question_image_label.setText("")
-        self.question_image_label.setPixmap(QtGui.QPixmap("question.png"))
+        self.question_image_label.setText('')
+        self.question_image_label.setPixmap(QtGui.QPixmap('question.png'))
         self.question_image_label.setScaledContents(True)
-        self.question_image_label.setObjectName("question_image_label")
+        self.question_image_label.setObjectName('question_image_label')
 
         self.about_label_btn = QtWidgets.QLabel(self.about_btn_widget)
         self.about_label_btn.setGeometry(QtCore.QRect(50, 80, 51, 20))
-        self.about_label_btn.setObjectName("about_label_btn")
+        self.about_label_btn.setObjectName('about_label_btn')
 
         self.evaluator_btn_widget = QtWidgets.QWidget(self.sidebar)
         self.evaluator_btn_widget.setGeometry(QtCore.QRect(0, 0, 130, 111))
-        self.evaluator_btn_widget.setStyleSheet("background: #212c38;")
-        self.evaluator_btn_widget.setObjectName("evaluator_btn_widget")
+        self.evaluator_btn_widget.setStyleSheet('background: #212c38;')
+        self.evaluator_btn_widget.setObjectName('evaluator_btn_widget')
 
         self.shield_image_label = QtWidgets.QLabel(self.evaluator_btn_widget)
         self.shield_image_label.setGeometry(QtCore.QRect(30, 10, 80, 70))
-        self.shield_image_label.setText("")
-        self.shield_image_label.setPixmap(QtGui.QPixmap("shield.png"))
+        self.shield_image_label.setText('')
+        self.shield_image_label.setPixmap(QtGui.QPixmap('shield.png'))
         self.shield_image_label.setScaledContents(True)
-        self.shield_image_label.setObjectName("shield_image_label")
+        self.shield_image_label.setObjectName('shield_image_label')
         
         self.evaluator_label_btn = QtWidgets.QLabel(self.evaluator_btn_widget)
         self.evaluator_label_btn.setGeometry(QtCore.QRect(40, 80, 70, 20))
-        self.evaluator_label_btn.setObjectName("evaluator_label_btn")
+        self.evaluator_label_btn.setObjectName('evaluator_label_btn')
 
         self.stackedWidget = QtWidgets.QStackedWidget(USBEvaluatorGui)
         self.stackedWidget.setGeometry(QtCore.QRect(130, 0, 670, 480))
-        self.stackedWidget.setObjectName("stackedWidget")
+        self.stackedWidget.setObjectName('stackedWidget')
 
         self.evaluator_page = QtWidgets.QWidget()
-        self.evaluator_page.setObjectName("evaluator_page")
+        self.evaluator_page.setObjectName('evaluator_page')
 
         self.logs_text_box = QtWidgets.QPlainTextEdit(self.evaluator_page)
         self.logs_text_box.setGeometry(QtCore.QRect(7, 260, 650, 210))
 
         font = QtGui.QFont()
-        font.setFamily("Nirmala UI")
+        font.setFamily('Nirmala UI')
         font.setPointSize(SMALL_FONT)
         font.setBold(False)
         font.setItalic(False)
         font.setWeight(50)
 
         self.logs_text_box.setFont(font)
-        self.logs_text_box.setStyleSheet("font: 10pt;")
+        self.logs_text_box.setStyleSheet('font: {}pt;'.format(SMALL_FONT))
         self.logs_text_box.setReadOnly(True)
-        self.logs_text_box.setPlainText("")
-        self.logs_text_box.setObjectName("logs_text_box")
+        self.logs_text_box.setPlainText('')
+        self.logs_text_box.setObjectName('logs_text_box')
 
         self.logs_label = QtWidgets.QLabel(self.evaluator_page)
         self.logs_label.setGeometry(QtCore.QRect(10, 230, 60, 20))
-        self.logs_label.setObjectName("logs_label")
+        self.logs_label.setObjectName('logs_label')
 
         self.toggle_evaluator_btn = QtWidgets.QPushButton(self.evaluator_page)
         self.toggle_evaluator_btn.setGeometry(QtCore.QRect(240, 50, 150, 150))
         
         font = QtGui.QFont()
-        font.setFamily("Nirmala UI")
+        font.setFamily('Nirmala UI')
         font.setPointSize(LARGE_FONT)
         font.setBold(False)
         font.setItalic(False)
         font.setWeight(50)
 
         self.toggle_evaluator_btn.setFont(font)
-        self.toggle_evaluator_btn.setStyleSheet("QPushButton {\n"
-            "    font: 14pt;\n"
-            "    border: 2px solid green;\n"
-            "    border-radius: 75px;\n"
-            "    border-style: outset;\n"
-            "    background: #34495e;\n"
-            "    color: white;\n"
-            "    padding: 5px;\n"
-            "    }\n"
-            "\n"
-            "QPushButton:pressed {\n"
-            "    border-style: inset;\n"
-            "    background: #7ba8d6;\n"
-            "    }")
-        self.toggle_evaluator_btn.setObjectName("toggle_evaluator_btn")
+        self.toggle_evaluator_btn.setStyleSheet('QPushButton {\n'
+            '    font: 14pt;\n'
+            '    border: 2px solid green;\n'
+            '    border-radius: 75px;\n'
+            '    border-style: outset;\n'
+            '    background: #34495e;\n'
+            '    color: white;\n'
+            '    padding: 5px;\n'
+            '    }\n'
+            '\n'
+            'QPushButton:pressed {\n'
+            '    border-style: inset;\n'
+            '    background: #7ba8d6;\n'
+            '    }')
+        self.toggle_evaluator_btn.setObjectName('toggle_evaluator_btn')
 
         self.stackedWidget.addWidget(self.evaluator_page)
         
         self.about_page = QtWidgets.QWidget()
-        self.about_page.setObjectName("about_page")
+        self.about_page.setObjectName('about_page')
         
         self.stackedWidget.addWidget(self.about_page)
 
@@ -133,14 +133,14 @@ class HeimdallMainWindow(object):
 
     def retranslateUi(self, USBEvaluatorGui):
         _translate = QtCore.QCoreApplication.translate
-        USBEvaluatorGui.setWindowTitle(_translate("USBEvaluatorGui", "USBEvaluatorGui"))
+        USBEvaluatorGui.setWindowTitle(_translate('USBEvaluatorGui', 'USBEvaluatorGui'))
 
         self.toggle_evaluator_btn.clicked.connect(self.__toggle_evaluator)
 
-        self.about_label_btn.setText(_translate("USBEvaluatorGui", "About"))
-        self.evaluator_label_btn.setText(_translate("USBEvaluatorGui", "Evaluator"))
-        self.logs_label.setText(_translate("USBEvaluatorGui", "Logs:"))
-        self.toggle_evaluator_btn.setText(_translate("USBEvaluatorGui", "Start"))
+        self.about_label_btn.setText(_translate('USBEvaluatorGui', 'About'))
+        self.evaluator_label_btn.setText(_translate('USBEvaluatorGui', 'Evaluator'))
+        self.logs_label.setText(_translate('USBEvaluatorGui', 'Logs:'))
+        self.toggle_evaluator_btn.setText(_translate('USBEvaluatorGui', 'Start'))
 
     def normal_write_text(self, text):
         cursor = self.logs_text_box.textCursor()
