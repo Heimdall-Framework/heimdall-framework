db5d2c9e4c89154fc1ce5d10c19511f59bec9f45
Ivan Zlatanov <ivanff1@abv.bg>
2021-01-26 22:28:51 +0200

    A single comment was updated.

--- heimdall_framework/modules/file_operations_provider.py
+++ heimdall_framework/modules/file_operations_provider.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import mmap
 import filecmp
 import subprocess
@@ -14,20 +14,21 @@ class FileOperationsProvider():
         :param first_file_path: The path to first file that will be compared
         :param second_file_path: The path to first file that will be compared
         """
-        
+
         return filecmp.cmp(first_file_path, second_file_path)
 
     def find_file(self, configuration, directory, file):
         """
         Recursively search for a specific file.
 
-        :param directory: The directory that will be searched
+        :param configuration: A FrameworkConfiguration instance
         :param file: The file that it is being looked for
         """
 
         for dir_path, directories, files in os.walk(directory, followlinks=True):
             for sub_dir in directories:
-                self.find_file(configuration, configuration.mounting_point + sub_dir, file)
+                self.find_file(
+                    configuration, configuration.mounting_point + sub_dir, file)
 
             for file_name in files:
                 if file_name.lower == str(file).lower:
@@ -44,7 +45,8 @@ class FileOperationsProvider():
         """
 
         with open(file_path) as file:
-            file_string_object = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
+            file_string_object = mmap.mmap(
+                file.fileno(), 0, access=mmap.ACCESS_READ)
             return file_string_object.find(string) != -1
 
     def create_img_file(self, logger, extension):
@@ -54,12 +56,14 @@ class FileOperationsProvider():
         :param extension: The file extension that will be used (iso or img) 
         """
 
-        creation_command = 'mkisofs -o /tmp/temp_image.{} {}'.format(extension, os.environ['DEVS_MOUNTPOINT'])
-        process = subprocess.Popen(creation_command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
+        creation_command = 'mkisofs -o /tmp/temp_image.{} {}'.format(
+            extension, os.environ['DEVS_MOUNTPOINT'])
+        process = subprocess.Popen(creation_command.split(
+        ), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
         output, error = process.communicate()
         if error != None:
             logger.log(error)
             return False
-        
+
         return True
