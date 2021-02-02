ef32595ad4b8e8b6b80cec25e18eb3c043fe4da7
Ivan Zlatanov <ivanff1@abv.bg>
2021-02-02 22:08:23 +0200

    Comment in logger was updated.

--- heimdall_framework/modules/logger.py
+++ heimdall_framework/modules/logger.py
@@ -1,6 +1,7 @@
-import os 
+import os
 import sys
 from datetime import datetime
+
 
 class Logger():
     def __init__(self, logs_directory_path: str):
@@ -8,21 +9,23 @@ class Logger():
 
     def log(self, string, silent=False):
         current_datetime = datetime.now()
-        
+
         if not os.path.exists(self.__logs_directory_path):
             os.mkdir(self.__logs_directory_path)
-        
-        logfile_name = '%s/LOG_%s_%s_%s'%(self.__logs_directory_path, current_datetime.year, current_datetime.month, current_datetime.day)
-        
-        file = open(logfile_name + '.log' , 'a+') 
 
-        file.write('[' + str(current_datetime.timestamp()) + ']' + string  + '\r\n')
-        
-        # doesn't print the log if the silent flag is set to True
+        logfile_name = '%s/LOG_%s_%s_%s' % (self.__logs_directory_path,
+                                            current_datetime.year, current_datetime.month, current_datetime.day)
+
+        file = open(logfile_name + '.log', 'a+')
+
+        file.write(
+            '[' + str(current_datetime.timestamp()) + ']' + string + '\r\n')
+
+        # Does not print the log if the silent flag is raised
         if not silent:
             print(string)
 
-        file.close()    
+        file.close()
 
     def log_error(self, *args, **kwargs):
         print(*args, file=sys.stderr, **kwargs)
